0315106bc27c0c19cbde1e21c4de80c140114b38
André Costa <costandre01@gmail.com>
2026-05-09 14:09:22 +0100

    fix threejs imports

--- Aplicacao/assets/main.js
+++ Aplicacao/assets/main.js
@@ -1,6 +1,10 @@
-import * as THREE from 'three';
+/*import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+*/
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
 /* ============================================================
    TEXTURAS (SEM ANIMAÇÃO)
